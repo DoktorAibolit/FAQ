d2fd4341fdc669cc759d4fd9478907aec14d306e
DoktorAibolit <84208211+DoktorAibolit@users.noreply.github.com>
2021-06-10 12:40:48 -0400

    Create script_search_word.ps1

---
+++ script_search_word.ps1
@@ -0,0 +1,15 @@
+# Comment ça marche?
+# 1.Précisez le mot que vous recherchez (dans notre cas c'est un Tibidou )
+# 2.Précisez le chemin en format: C:\Dossier\dosier ou \\serveur\Dossier\Dossier
+#           (dans notre cas C:\Temp\log)  
+# 3.Spécifiez le format de fichier dans laequel vous souhaitez rechercher
+#           (dans notre cas tous les fichiers avec extension txt: *.txt) 
+ 
+$searchWords = 'Tibidou'
+
+Foreach ($sw in $searchWords)
+{
+    Get-Childitem -Path "C:\Temp\log" -Recurse -include "*.txt" | 
+    Select-String -Pattern "$sw" | 
+    Select-Object Filename,LineNumber,@{n='SearchWord';e={$sw}}
+}
