68df9cd17c061ffe34708706b264f66e85b24e53
DoktorAibolit <84208211+DoktorAibolit@users.noreply.github.com>
2021-06-10 12:45:33 -0400

    Update script_search_word.ps1

--- script_search_word.ps1
+++ script_search_word.ps1
@@ -1,11 +1,12 @@
-##### Un script qui pour vous permet de trouver des fichiers contenant un mot cpécifique.##### 
+##### Un script qui pour vous permet de trouver des fichiers contenant un mot cpécifique.#####
+#
 # Comment ça marche?
 # 1.Précisez le mot que vous recherchez (dans notre cas c'est un Tibidou )
 # 2.Précisez le chemin en format: C:\Dossier\dosier ou \\serveur\Dossier\Dossier
 #           (dans notre cas C:\Temp\log)  
 # 3.Spécifiez le format de fichier dans laequel vous souhaitez rechercher
 #           (dans notre cas tous les fichiers avec extension txt: *.txt) 
- 
+# 
 $searchWords = 'Tibidou'
 
 Foreach ($sw in $searchWords)
